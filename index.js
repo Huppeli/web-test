61a70b896bfa618128edf903d6612686f91d9ef8
Mika Manninen <mika.manninen93@gmail.com>
2018-05-04 10:30:46 +0300

    Now with fancy arrays

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ request({url:'http://liiga.fi/tilastot/2017-2018/playoffs/pelaajat/',encoding:'u
   if (!error && response.statusCode == 200) {
     // test out console.log(html);
     var $ = cheerio.load(html);
+    var results = [];
 
     var stuff = $('#stats').find("tbody>tr").each(function(){
       var stuff2 = $(this).find("a").text();
-      console.log(stuff2);
+      var metadata = {
+        name: stuff2
+      };
+      results.push(metadata);
     })
+    console.log(results);
   }
 })
