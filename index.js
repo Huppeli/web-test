0ebc8f9be3c03a7956d9155a3d7756e38fd119ae
Mika Manninen <mika.manninen93@gmail.com>
2018-05-04 10:17:33 +0300

    Working, prints out all names in a line

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-request('http://liiga.fi/tilastot/2017-2018/playoffs/pelaajat/', function (error, response, html){
+request({url:'http://liiga.fi/tilastot/2017-2018/playoffs/pelaajat/',encoding:'utf8'}, function (error, response, html){
   if (!error && response.statusCode == 200) {
     // test out console.log(html);
     var $ = cheerio.load(html);
 
-    var stuff = $('#stats').children('tbody').children('tr').each(function(i, element){
-
-    }).html();
-
-    // var stuff = $('#page').children('h1').html();
-    console.log(stuff);
+    var stuff = $('#stats').find("tbody>tr").each(function(){
+      var stuff2 = $(this).find("a").text();
+      console.log(stuff2);
+    })
   }
 })
